ee64e06ab95e896a2aef5c5be0468f2062ed8075
jlpoelma <jonathan.poelman@ugent.be>
2022-02-05 20:49:28 +0100

    added Non negative and Aged Brie test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
@@ -29,6 +30,25 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals(-3, app.items[0].sellIn);
         assertEquals(18, app.items[0].quality);
+    }
+
+    @Test
+    void assertNonNegative() {
+        Item[] items = new Item[] { new Item("foo", 10, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality >= 0);
+    }
+
+    @Test
+    void updateAgedBrie(){
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 10), new Item("Aged Brie", -4, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(11, app.items[0].quality);
+        assertEquals(-5, app.items[1].sellIn);
+        assertEquals(12, app.items[1].quality);
     }
 
 }
