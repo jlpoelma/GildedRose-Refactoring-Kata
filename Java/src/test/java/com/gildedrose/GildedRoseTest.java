3b670c5de8e6618e2ffafa63d2bf212cfbd90c7f
jlpoelma <jonathan.poelman@ugent.be>
2022-02-05 20:37:18 +0100

    Fixed foo test and added base item update tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,24 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+    }
+    @Test
+    void updateBaseItem() {
+        Item[] items = new Item[] { new Item("foo", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(19, app.items[0].quality);
+    }
+
+    @Test
+    void updateExpiredBaseItem() {
+        Item[] items = new Item[] { new Item("foo", -2, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-3, app.items[0].sellIn);
+        assertEquals(18, app.items[0].quality);
     }
 
 }
