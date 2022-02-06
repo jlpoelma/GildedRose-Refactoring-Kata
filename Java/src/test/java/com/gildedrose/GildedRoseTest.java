1955cbd57fc29d64e931dfbef6974e33d5873ca0
jlpoelma <jonathan.poelman@ugent.be>
2022-02-06 13:15:54 +0100

    Implemented some additional edge case tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,6 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
+    public static int QUALITY_UPPER_LIMIT = 50;
+    public static int QUALITY_LOWER_LIMIT = 0;
+
     @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
@@ -17,36 +20,68 @@ class GildedRoseTest {
     @Test
     void updateBaseItem() {
         String name = "foo";
-        Item[] items = new Item[] { new Item(name, 10, 20),
-                                    new Item(name, -2, 20),
-                                    new Item(name, 10, 0)};
-        Item[] updatedItems = new Item[] {  new Item(name, 9, 19),
-                                            new Item(name, -3, 18),
-                                            new Item(name, 9, 0)};
+        Item[] items = new Item[] { new Item(name, 10, 20)};
+        Item[] updatedItems = new Item[] {  new Item(name, 9, 19)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        for (int i=0; i < app.items.length; i++){
-            assertEquals(updatedItems[i].quality, app.items[i].quality);
-            assertEquals(updatedItems[i].sellIn, app.items[i].sellIn);
-        }
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
     }
 
+    @Test
+    void updateExpiredBaseItem(){
+        String name = "foo";
+        Item[] items = new Item[] { new Item(name, -2, 20)};
+        Item[] updatedItems = new Item[] {new Item(name, -3, 18)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
+    }
 
+    @Test
+    void assertLowerLimitBaseItem(){
+        String name = "foo";
+        Item[] items = new Item[] {new Item(name, 10, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality >= QUALITY_LOWER_LIMIT);
+        assertEquals(9, app.items[0].sellIn);
+    }
 
     @Test
     void updateAgedBrie(){
         String name = "Aged Brie";
-        Item[] items = new Item[] { new Item(name, 10, 10),
-                                    new Item(name, -4, 10) };
-        Item[] updatedItems = new Item[] {  new Item(name, 9, 11),
-                                            new Item(name, -5, 12) };
+        Item[] items = new Item[] { new Item(name, 10, 10) };
+        Item[] updatedItems = new Item[] {  new Item(name, 9, 11) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        for (int i=0; i < app.items.length; i++){
-            assertEquals(updatedItems[i].quality, app.items[i].quality);
-            assertEquals(updatedItems[i].sellIn, app.items[i].sellIn);
-        }
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
     }
+
+    @Test
+    void updateExpiredAgedBrie(){
+        String name = "Aged Brie";
+        Item[] items = new Item[] {new Item(name, -4, 10) };
+        Item[] updatedItems = new Item[] {new Item(name, -5, 12) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
+    }
+
+    @Test
+    void assertUpperLimitAgedBrie(){
+        String name = "Aged Brie";
+        Item[] items = new Item[] { new Item(name, -5, 49)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality <= QUALITY_UPPER_LIMIT);
+        assertEquals(-6, app.items[0].sellIn);
+    }
+
+
 
     @Test
     void updateSulfuras(){
@@ -55,36 +90,104 @@ class GildedRoseTest {
         Item[] updatedItems = new Item[] { new Item(name, 10, 80)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        for (int i=0; i < app.items.length; i++){
-            assertEquals(updatedItems[i].quality, app.items[i].quality);
-            assertEquals(updatedItems[i].sellIn, app.items[i].sellIn);
-        }
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
     }
 
     @Test
     void updateBackstage(){
         String name = "Backstage passes to a TAFKAL80ETC concert";
-        Item[] items = new Item[] { new Item(name, 15, 20),
-                                    new Item(name, 10, 20),
-                                    new Item(name, 5, 20),
-                                    new Item(name, 0, 20)};
-        Item[] updatedItems = new Item[] {  new Item(name, 14, 21),
-                                            new Item(name, 9, 22),
-                                            new Item(name, 4, 23),
-                                            new Item(name, -1, 0)};
+        Item[] items = new Item[] { new Item(name, 15, 20)};
+        Item[] updatedItems = new Item[] {  new Item(name, 14, 21)};
 
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        for (int i=0; i < app.items.length; i++){
-            assertEquals(updatedItems[i].quality, app.items[i].quality);
-            assertEquals(updatedItems[i].sellIn, app.items[i].sellIn);
-        }
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
+    }
+
+    @Test
+    void updateBackstage10(){
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        Item[] items = new Item[] {new Item(name, 10, 20)};
+        Item[] updatedItems = new Item[] {new Item(name, 9, 22),};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
+    }
+
+    @Test
+    void updateBackstage5(){
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        Item[] items = new Item[] {new Item(name, 5, 20)};
+        Item[] updatedItems = new Item[] { new Item(name, 4, 23)};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
+    }
+
+    @Test
+    void updateExpiredBackstage(){
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        Item[] items = new Item[] { new Item(name, 0, 20)};
+        Item[] updatedItems = new Item[] { new Item(name, -1, 0)};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
+    }
+
+    @Test
+    void assertUpperLimitBackstage(){
+        String name = "Backstage passes to a TAFKAL80ETC concert";
+        Item[] items = new Item[] {
+            new Item(name, 5, 48)};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality <= QUALITY_UPPER_LIMIT);
+        assertEquals(4, app.items[0].sellIn);
     }
 
     @Test
     void updateConjured(){
-
+        String name = "Conjured";
+        Item[] items = new Item[] { new Item(name, 10, 20)};
+        Item[] updatedItems = new Item[] {  new Item(name, 9, 18)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
     }
+
+    @Test
+    void updateExpiredConjured(){
+        String name = "Conjured";
+        Item[] items = new Item[] { new Item(name, -2, 20)};
+        Item[] updatedItems = new Item[] { new Item(name, -3, 16)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(updatedItems[0].quality, app.items[0].quality);
+        assertEquals(updatedItems[0].sellIn, app.items[0].sellIn);
+    }
+
+    @Test
+    void assertLowerLimitConjured(){
+        String name = "Conjured";
+        Item[] items = new Item[] { new Item(name, 10, 1)};
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertTrue(app.items[0].quality >= QUALITY_LOWER_LIMIT);
+        assertEquals(9, app.items[0].sellIn);
+    }
+
+
 
 
 }
